8dccb53ac58be827a40741bffb481d640e1fba77
bruno <brunocalegari7@gmail.com>
2024-07-06 20:15:53 -0300

    update classe: custom user details service

--- src/main/java/com/example/login_auth_api/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
